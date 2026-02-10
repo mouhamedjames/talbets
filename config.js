fa0013e339d753469235f1d14e05b1beda3fefa0
mouhamedjames <81452452+mouhamedjames@users.noreply.github.com>
2026-02-10 21:29:02 +0100

    Update Telegram bot token and chat ID

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 module.exports = {
     // Telegram Bot Configuration
     telegram: {
-        botToken: process.env.TELEGRAM_BOT_TOKEN || '8400132592:AAFoz2e2d60iLFcLBXEX3077BAlWjVdh85I',
-        chatId: process.env.TELEGRAM_CHAT_ID || '-5200817415'
+        botToken: process.env.TELEGRAM_BOT_TOKEN || '8339822460:AAH0henAILHylbYCj9Ac9Hqpz0OeNiURrKQ',
+        chatId: process.env.TELEGRAM_CHAT_ID || '-5282262655'
     },
     
     // Server Configuration
